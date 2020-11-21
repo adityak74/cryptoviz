b30a4b12a2226bb1bda6add5120a363645be4ef1
Aditya Karnam <akarnam37@gmail.com>
2020-11-21 09:17:32 +0530

    feat: migrations

---
+++ migrations/20201118120521-create-coinsdata-table.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('CoinsData', {
+      id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+			},
+      coinID: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'WazirXCoins', // name of Target model
+          key: 'id', // key in Target model that we're referencing
+        },
+      },
+      low: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      high: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      last: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      open: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      volume: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      sell: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      buy: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+      },
+      tickerType: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+      updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('CoinsData');
+  }
+};
