51e90a3448f069eb4a011ae1ecadeacf43db0c64
Aditya Karnam <akarnam37@gmail.com>
2021-06-09 20:36:50 -0500

    feat: coindcx api tests

--- tests/coindcx-test.js
+++ tests/coindcx-test.js
@@ -25,11 +25,34 @@ const marketDetailsShape = {
     "status": "active"
 };
 
+// sample data we will only compare the keys here
+const marketTickerShape = {
+    market: "BTCINR",
+    change_24_hour: "11.565",
+    high: "2880000.0",
+    low: "2508036.98",
+    volume: "104871254.3999999712",
+    last_price: "2851176.790000",
+    bid: "2846000",
+    ask: "2851046.84",
+    timestamp: 1623288922
+};
+
 describe('CoinDCX Market Details Async', function () {
-    it('should return valid response', function () {
+    it('response shape should match', function () {
         marketDetailsAsync(function(error, response) {
             const responseObject = JSON.parse(response.text);
             assert.deepEqual(Object.keys(marketDetailsShape), Object.keys(responseObject[0]));
+        });
+    });
+});
+
+
+describe('CoinDCX Market Ticker Async', function () {
+    it('response shape should match', function () {
+        marketTickerAsync(function(error, response) {
+            const responseObject = JSON.parse(response.text);
+            assert.deepEqual(Object.keys(marketTickerShape), Object.keys(responseObject[0]));
         });
     });
 });
