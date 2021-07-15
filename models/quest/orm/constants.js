ec952ae4fe1990a327fad97894cd8a9821b429c7
Aditya Karnam <akarnam37@gmail.com>
2021-07-15 02:33:44 -0500

    feat: orm layer init

---
+++ models/quest/orm/constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'BINARY': 0,
+    'BOOLEAN': 1,
+    'DATE': 2,
+    'DOUBLE': 3,
+    'FLOAT': 4,
+    'INT': 5,
+    'LONG': 6,
+    'LONG256': 7,
+    'SHORT': 8,
+    'STRING': 9,
+    'SYMBOL': 10,
+    'TIMESTAMP': 11,
+};
---
+++ models/quest/orm/types.js
@@ -0,0 +1,20 @@
+const constants = require('./constants');
+
+const {
+    BINARY,
+    BOOLEAN,
+    DATE,
+    DOUBLE,
+    FLOAT,
+    INT,
+    LONG,
+    LONG256,
+    SHORT,
+    STRING,
+    SYMBOL,
+    TIMESTAMP,
+} = constants;
+
+module.exports = {
+    
+};
