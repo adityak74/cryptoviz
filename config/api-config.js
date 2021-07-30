dc644071b0a2c66d6cb3163c4afcbca6e76e7eb4
Aditya Karnam <akarnam37@gmail.com>
2021-07-30 18:40:32 -0500

    chore: cleanup test routes

--- config/api-config.js
+++ config/api-config.js
@@ -5,20 +5,12 @@ const path  = require('path');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 const moment = require('moment');
-const coinsDataSeeder = require('../services/coinsData-seeder');
+const coinsDataSeeder = require('../services/wazirx/coinsData-seeder');
 const { sql } = require('../utils');
 
 if (!process.env.GCP_PROJECT) {
   dotenv.config({ path: path.join(__dirname, '..', `.env.${process.env.NODE_ENV || 'development'}`) });
 }
-
-const {
-  TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN,
-  WHATSAPP_FROM,
-  WHATSAPP_TO,
-} = process.env;
-const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
  
  app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -69,37 +61,6 @@ app.get('/coins', async (req, res) => {
       success: false,
     });
   }
-});
-
-const formattedCoinsDataMessage = (coin, coinData) => 
-  `Coin ${coin.name} values at a high of ${coinData.high} and low of ${coinData.low} at ${moment(coinData.createdAt).format('MMMM Do YYYY, h:mm:ss a')}`;
-
-const notifyUserForCoin = (coinBaseUnit, res) => {
-  const { select } = sql;
-  const { selectWazirXCoinsByPredicate, selectCoinsDataByPredicate } = select;
-  selectWazirXCoinsByPredicate({ base_unit: coinBaseUnit })
-    .then((record) => {
-      const wazirxCoin = record.dataValues;
-      selectCoinsDataByPredicate({ coinID: wazirxCoin.id }, [[ 'createdAt', 'DESC' ]])
-        .then((coinRecord) => {
-          const latestCoinPrice = coinRecord[0].dataValues;
-          client.messages
-            .create({
-              body: formattedCoinsDataMessage(wazirxCoin, latestCoinPrice),
-              from: WHATSAPP_FROM,
-              to: WHATSAPP_TO
-            })
-            .then(message => res.send({
-                messageId: message.sid,
-                success: true,
-              }))
-            .done();
-        });
-    });
-};
-
-app.post('/notifyUser', (req, res) => {
-  notifyUserForCoin('link', res);
 });
 
 module.exports = {
