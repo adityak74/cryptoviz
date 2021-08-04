0ba65ce1de7b0eaf4c6c6e53e176fe857c6af310
Aditya Karnam <akarnam37@gmail.com>
2021-08-04 18:34:09 -0500

    chore: correct runner path

--- utils/runners/coinsData-runner.js
+++ utils/runners/coinsData-runner.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const path = require('path');
 const shell = require('shelljs');
 
-const seederPath = path.join(`${__dirname}`, '..', '..', 'services', 'coinsData-seeder.js');
+const seederPath = path.join(`${__dirname}`, '..', '..', 'services', 'wazirx', 'coinsData-seeder.js');
  
 cron.schedule('*/5 * * * *', () => {
   console.log('Updating database with coins data');
