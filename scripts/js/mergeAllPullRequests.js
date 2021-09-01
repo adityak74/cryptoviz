3094ba78a43f537bd098983ad0cd8b17ef64f508
Aditya Karnam <akarnam37@gmail.com>
2021-09-01 17:15:59 -0500

    chore: merge all pr script

--- scripts/js/mergeAllPullRequests.js
+++ scripts/js/mergeAllPullRequests.js
@@ -1,4 +1,4 @@
-const { Octokit, App, Action } = require("octokit");
+const { Octokit } = require("octokit");
 const dotenv = require('dotenv');
 const path = require('path');
 
@@ -9,9 +9,25 @@ const octokit = new Octokit({ auth: process.env.GIT_PAT });
 
 // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
 async function mergeAllPullRequests() {
-  const { data } = await octokit.rest.users.getAuthenticated();
-  console.log("Hello", data);
-  
+  await octokit.rest.users.getAuthenticated();
+  octokit.rest.pulls.list({
+    owner: "adityak74",
+    repo: "cryptoviz",
+    state: "open"
+  }).then(async ({ data }) => {
+    for(let i = 0; i < data.length; i++) {
+      console.info('Merging PR', data[i].number);
+      const mergeData = await octokit.rest.pulls.merge({
+        owner: "adityak74",
+        repo: "cryptoviz",
+        pull_number: data[i].number,
+        merge_method: "squash"
+      });
+      console.info(`${mergeData.data.message} with hash : ${mergeData.data.sha}`);
+    }
+  }).catch(err => {
+    console.error('Error listing pull requests', err);
+  });
 } 
 
 mergeAllPullRequests();
