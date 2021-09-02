7b4791529a23c8b89a73622f295becc0e46337fd
Aditya Karnam <akarnam37@gmail.com>
2021-09-01 21:19:39 -0500

    chore: add message queue

--- scripts/js/mergeAllPullRequests.js
+++ scripts/js/mergeAllPullRequests.js
@@ -8,26 +8,37 @@ dotenv.config({ path: path.join(__dirname, '..', '..', `.env.${process.env.NODE_
 const octokit = new Octokit({ auth: process.env.GIT_PAT });
 
 // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
-async function mergeAllPullRequests() {
+(async function mergeAllPullRequests() {
+  const repo = 'cryptoviz';
   await octokit.rest.users.getAuthenticated();
   octokit.rest.pulls.list({
     owner: "adityak74",
-    repo: "cryptoviz",
+    repo,
     state: "open"
   }).then(async ({ data }) => {
-    for(let i = 0; i < data.length; i++) {
-      console.info('Merging PR', data[i].number);
-      const mergeData = await octokit.rest.pulls.merge({
-        owner: "adityak74",
-        repo: "cryptoviz",
-        pull_number: data[i].number,
-        merge_method: "squash"
-      });
-      console.info(`${mergeData.data.message} with hash : ${mergeData.data.sha}`);
+    let pullRequestMergeQueue = [];
+    pullRequestMergeQueue = data.map(pullRequest => pullRequest.number);
+    if (!pullRequestMergeQueue.length) process.exit(0);
+    console.log('Attempting to merge pull requests: ', pullRequestMergeQueue.length);
+    while (pullRequestMergeQueue.length > 0) {
+      const pullRequestNumber = pullRequestMergeQueue.shift();
+      console.info('Merging PR', pullRequestNumber);
+      try {
+        const mergeData = await octokit.rest.pulls.merge({
+          owner: "adityak74",
+          repo,
+          pull_number: pullRequestNumber,
+          merge_method: "squash"
+        });
+        console.info(`${mergeData.data.message} with hash : ${mergeData.data.sha}`);
+      } catch (error) {
+        console.error(error);
+        pullRequestMergeQueue = pullRequestMergeQueue.concat(pullRequestNumber);
+        continue;
+      }
+      console.log('Remaining PRs: ', pullRequestMergeQueue.length);
     }
   }).catch(err => {
     console.error('Error listing pull requests', err);
   });
-} 
-
-mergeAllPullRequests();
+})();
