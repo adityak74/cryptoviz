250a4534ced19a18d940ac4b4c7772513db40b13
Aditya Karnam <akarnam37@gmail.com>
2021-11-03 15:07:09 -0500

    fix: sequlize timeout error config

--- models/index.js
+++ models/index.js
@@ -23,6 +23,12 @@ sequelize = new Sequelize(MYSQL_DATABASE, SQL_USER, MYSQL_ROOT_PASSWORD, {
   port: SQL_PORT,
   dialect: 'mysql',
   dialectModule: mysql2,
+  pool: {
+    max: 100,
+    min: 0,
+    idle: 200000,
+    acquire: 1000000,
+  },
 });
 
 fs
