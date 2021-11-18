2c7aebf603b3a9e77e01532c1a523a7ba69577c6
Aditya Karnam <akarnam37@gmail.com>
2021-11-18 17:10:00 -0600

    feat: linode pulumi

--- cryptoviz-linode-pulumi/index.ts
+++ cryptoviz-linode-pulumi/index.ts
@@ -1,12 +1,15 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as linode from "@pulumi/linode";
 
 // Create a Linode resource (Linode Instance)
-const instance = new linode.Instance("my-instance", {
-    type: "g6-nanode-1",
-    region: "us-east",
+const instance = new linode.Instance("cryptoviz-us-instance-new", {
+    authorizedUsers: ["adityak74"],
+    type: "g6-standard-1",
+    privateIp: true,
+    region: "us-southeast",
     image: "linode/ubuntu18.04",
+    label: "cryptoviz-us-southeast",
+    rootPass: "testpass!",
 });
 
 // Export the Instance label of the instance
-export const instanceLabel = instance.label;
+export const instanceLabel = instance;
