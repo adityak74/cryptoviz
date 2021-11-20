24be76b292a65ba15bcd9ab886152fc5cefc90b1
Aditya Karnam <akarnam37@gmail.com>
2021-11-20 14:40:13 -0600

    chore: exclude options

--- config/api-config.js
+++ config/api-config.js
@@ -39,7 +39,7 @@ if (!process.env.GCP_PROJECT) {
   dotenv.config({ path: path.join(__dirname, '..', `.env.${process.env.NODE_ENV || 'development'}`) });
 }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
