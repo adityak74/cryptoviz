fae5a7490a29c32efe2a381c8cbf1dcf8bf74099
Aditya Karnam <akarnam37@gmail.com>
2021-11-21 20:50:59 -0600

    fix: move dotenv to root

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const apis = require("./config/api-config");
+const path  = require('path');
+const dotenv = require('dotenv');
 const PORT = 8081;
 const { sql } = require('./utils');
+
+if (!process.env.GCP_PROJECT) {
+    dotenv.config({ path: path.join(__dirname, '..', `.env.${process.env.NODE_ENV || 'development'}`) });
+}
 
 apis.app.listen(process.env.PORT || PORT, async function() {
     const { select } = sql;
